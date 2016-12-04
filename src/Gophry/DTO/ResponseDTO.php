0876a3e03b97d1978a20dc207f20cf28d109aded
Norbert Kranitz <nkranitz@nkranitz05.szintezis-net.local>
2016-12-04 22:32:22 +0100

    Added reponse DTO improvements

--- src/Gophry/DTO/ResponseDTO.php
+++ src/Gophry/DTO/ResponseDTO.php
@@ -8,7 +8,23 @@ abstract class ResponseDTO extends DTO implements ResponseDTOInterface {
         $properties = get_object_vars($this);
         $result = array();
         foreach ($properties as $property => $value) {
-            $result[$property] = $value instanceof ResponseDTOInterface ? $value->toArray() : $value;
+            if ($value !== null) {
+                if ($value instanceof IResponseDTO) {
+                    $result[$property] = $value->toArray();  
+                } else if (is_array($value)) {
+                    if (array_keys($value) !== range(0, count($value) - 1)) {
+                        $result[$property] = $value;
+                    } else {
+                        $array = [];
+                        foreach ($value as $item) {
+                            $array[] = $item instanceof IResponseDTO ? $item->toArray() : $item;
+                        }
+                        $result[$property] = $array;
+                    }
+                } else {
+                    $result[$property] = $value;
+                }
+            }
         }
         return $result;
     }
